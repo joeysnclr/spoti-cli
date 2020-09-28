2cfdd49e788096a27b9da2e8bd93ddd919a90b35
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-09-27 21:25:48 -0700

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="spoti-cli",
-    version="0.0.1",
+    version="0.0.2",
     description="Terminal based Spotify client designed to use Vim keybindings.",
     long_description=README,
     long_description_content_type="text/markdown",

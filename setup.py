d2b9e1d1274771343eff4919c7537a94f74f4d1b
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-09-27 19:48:23 -0700

    pip entry points

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=["beautifulsoup4", "blessed", "Flask", "requests"],
     entry_points={
         "console_scripts": [
-            "spoti-cli=spoti-cli.__main__.py",
+            "spoti-cli=spoti-cli:__main__.py",
         ]
     },
 ) 
